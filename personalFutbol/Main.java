7b6aacb1c73c6fd4cd426c35f4f6b33965f5b3ab
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-05-26 17:17:45 -0600

    Se modifico el main

--- personalFutbol/Main.java
+++ personalFutbol/Main.java
@@ -2,17 +2,17 @@ public class Main extends Persona{
 
         public static void main(String[] args) {
         Futbolista jugador = new Futbolista();
-        jugador.nombre = "Benzema";
+        jugador.nombre = "Ronaldinho";
         jugador.viajar();
         jugador.concentrarse();
 
         Entrenador tecnico = new Entrenador();
-        tecnico.nombre = "Xavi";
+        tecnico.nombre = "Segundo Castillo";
         tecnico.viajar();
         tecnico.dirigirPartido();
 
         Masajista masajista = new Masajista();
-        masajista.nombre = "Messi";
+        masajista.nombre = "Cryuft";
         masajista.viajar();
         masajista.darMasaje();
     }
