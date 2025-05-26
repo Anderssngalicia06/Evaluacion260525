a8269f2605a5468ec48abc331a7ec5b2dc2898f1
Anderssngalicia06 <andyssjrmz.18@gmail.com>
2025-05-26 17:16:35 -0600

    Se crearon las demas clases restantes

--- personalFutbol/Main.java
+++ personalFutbol/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+public class Main extends Persona{
 
-    
+        public static void main(String[] args) {
+        Futbolista jugador = new Futbolista();
+        jugador.nombre = "Benzema";
+        jugador.viajar();
+        jugador.concentrarse();
+
+        Entrenador tecnico = new Entrenador();
+        tecnico.nombre = "Xavi";
+        tecnico.viajar();
+        tecnico.dirigirPartido();
+
+        Masajista masajista = new Masajista();
+        masajista.nombre = "Messi";
+        masajista.viajar();
+        masajista.darMasaje();
+    }
 
 }
--- personalFutbol/Masajista.java
+++ personalFutbol/Masajista.java
@@ -1,3 +1,8 @@
-public class Masajista {
+
+public class Masajista extends Persona {
+
+    public void darMasaje() {
+        System.out.println(nombre + " está dando un masaje.");
+    }
 
 }
